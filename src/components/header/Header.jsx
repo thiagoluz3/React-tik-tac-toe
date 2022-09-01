669387b4526b75ad817ab0ec8104289f176a0de7
Thiago Luz <thiago.henriqueluz@gmail.com>
2022-08-31 22:27:34 -0300

    Criando componente de ícone

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,16 @@
 import Title from "../Title/Title"
 import Subtitle from "../Subtitle/Subtitle"
 import styles from './Header.module.css'
+import Icon from "../icon/Icon"
 
 function Header () {
   return (
     <div className={styles.header}>
       <Title>Jogo da Velha</Title>
-    <Subtitle>Criado por Thiago Luz</Subtitle>
+      <Subtitle>Criado por Thiago Luz</Subtitle>
+      <div className={styles.iconContent}>
+        <Icon iconName="github" Link="https://github.com/thiagoluz3"/>
+      </div>
     </div>
     )
 }
---
+++ src/components/icon/Icon.jsx
@@ -0,0 +1,12 @@
+function Icon ({ iconName, size, Link }) {
+  return (
+    <a href={Link} target="_blank">  {/*target _blank faz com que ao clicar no logo vá para uma nova aba */}
+      <img src={`/icons/${iconName}.svg`} width={size}/>
+    </a>
+  )
+}
+
+Icon.defaultProps = {   /*mantem por padrão 30px caso não seja informado */
+  size: '30px'
+}
+export default Icon
