301eb1745b8f446d7c71b221d2d02e16191b4c3b
Thiago Luz <thiago.henriqueluz@gmail.com>
2022-09-06 20:43:13 -0300

    Criado condicional de winner

--- src/components/game/Game.jsx
+++ src/components/game/Game.jsx
@@ -19,6 +19,7 @@ function Game () {
   const [gameState, setGameState] = useState(Array(9).fill(0))
   const [currentPlayer, setCurrentPlayer] = useState(-1)
   const [winner, setWinner] = useState(0)
+  const [winnerLine, setWinnerLine] = useState([])
   const handleClick = (pos) => {
 
     if(gameState[pos]===0 && winner === 0) {
@@ -33,15 +34,22 @@ function Game () {
     winnerTable.forEach((line)=>{
       const values = line.map((pos) => gameState[pos])
       const sum = values.reduce((sum, value) => sum + value)
-      if (sum === 3 || sum === -3) setWinner(sum/3)
+      if (sum === 3 || sum === -3) {
+        setWinner(sum/3)
+        setWinnerLine(line)
+      }
     })
   }
 
   const handleReset = () => {
     setGameState(Array(9).fill(0))
     setWinner(0)
+    setWinnerLine([])
   }
   
+  const verifyWinnerLine = (pos) => 
+    winnerLine.find((value) => value === pos) !== undefined
+
   useEffect(() => {
     setCurrentPlayer(currentPlayer* -1)
     verifyGame()
@@ -56,7 +64,9 @@ function Game () {
             key={`game-option-pos-${pos}`}
             status={value}
             onClick={() => handleClick(pos)}
-          />)
+            isWinner= {verifyWinnerLine(pos)}
+          />
+        )
       }
       </div>
       <GameInfo 
