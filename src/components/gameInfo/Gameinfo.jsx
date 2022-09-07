ad75d517d02a6b55215f21d7c55dffd429981b0e
Thiago Luz <thiago.henriqueluz@gmail.com>
2022-09-06 22:06:14 -0300

    Correção de código

--- src/components/gameInfo/Gameinfo.jsx
+++ src/components/gameInfo/Gameinfo.jsx
@@ -13,7 +13,7 @@ function GameInfo ( {currentPlayer, winner, onReset, isDraw} ) {
             currentPlayer === 1 && <Icon iconName="circle" />
           }
           {
-            currentPlayer === -1 && <Icon iconName="X" />
+            currentPlayer === -1 && <Icon iconName="x" />
           }   
          </>
       }
@@ -25,7 +25,7 @@ function GameInfo ( {currentPlayer, winner, onReset, isDraw} ) {
               winner === 1 && <Icon iconName="circle" />
             }
             {
-              winner === -1 && <Icon iconName="X" />
+              winner === -1 && <Icon iconName="x" />
             }            
           </>
       }
